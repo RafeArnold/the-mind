5329f11dbe6a22a769729e342851b2c7d0720b9c
rafearnold <rafe.arnold@gmail.com>
2024-03-09 17:59:24 +0000

    implement basic single round game.

--- src/main/kotlin/uk/co/rafearnold/mind/Main.kt
+++ src/main/kotlin/uk/co/rafearnold/mind/Main.kt
@@ -1,5 +1,82 @@
 package uk.co.rafearnold.mind
 
-fun main() {
-  println("Hello World!")
+import java.util.UUID
+
+interface Server {
+
+  fun createLobby(): CreateLobbyResponse
+
+  fun joinLobby(lobbyId: String): Player
+
+  fun startGame(player: Player)
+
+  fun playCard(player: Player)
 }
+
+data class Player(val lobbyId: String, val isHost: Boolean, var state: ClientState = InLobby)
+
+sealed interface ClientState
+
+data object InLobby : ClientState
+
+data class InGame(val cards: MutableList<Card>) : ClientState
+
+data object GameWon : ClientState
+
+data object GameLost : ClientState
+
+data class Card(val value: Int)
+
+data class CreateLobbyResponse(val host: Player, val lobbyId: String)
+
+class SimpleServer : Server {
+
+  private val games: MutableList<Game> = mutableListOf()
+
+  override fun createLobby(): CreateLobbyResponse {
+    val lobbyId = UUID.randomUUID().toString()
+    val host = Player(lobbyId = lobbyId, isHost = true)
+    val game = Game(id = lobbyId, players = mutableListOf(host))
+    games.add(game)
+    return CreateLobbyResponse(host = host, lobbyId = lobbyId)
+  }
+
+  override fun joinLobby(lobbyId: String): Player {
+    val lobby = getGame(lobbyId = lobbyId)
+    val player = Player(lobbyId = lobbyId, isHost = false)
+    lobby.players.add(player)
+    return player
+  }
+
+  override fun startGame(player: Player) {
+    val game = getGame(player = player)
+    val deck = (1..100).shuffled().iterator()
+    for (@Suppress("NAME_SHADOWING") player in game.players) {
+      player.state = InGame(cards = mutableListOf(Card(deck.next())))
+    }
+  }
+
+  override fun playCard(player: Player) {
+    val game = getGame(player = player)
+    val removedCard = (player.state as InGame).cards.removeFirst()
+    if (game.players.all { (it.state as InGame).cards.isEmpty() }) {
+      game.setState(GameWon)
+    } else if (removedCard.value >
+      game.players.flatMap { (it.state as InGame).cards.map { card -> card.value } }.min()
+    ) {
+      game.setState(GameLost)
+    }
+  }
+
+  private fun Game.setState(state: ClientState) {
+    for (player in players) {
+      player.state = state
+    }
+  }
+
+  private fun getGame(player: Player): Game = getGame(lobbyId = player.lobbyId)
+
+  private fun getGame(lobbyId: String): Game = games.first { it.id == lobbyId }
+}
+
+private data class Game(val id: String, val players: MutableList<Player>)
---
+++ src/test/kotlin/uk/co/rafearnold/mind/Tests.kt
@@ -0,0 +1,76 @@
+package uk.co.rafearnold.mind
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
+
+class Tests {
+
+  @Test
+  fun `all cards played in correct order`() {
+    val server = SimpleServer()
+    val (host, lobbyId) = server.createLobby()
+    assertEquals(InLobby, host.state)
+    val player2 = server.joinLobby(lobbyId)
+    assertEquals(InLobby, player2.state)
+    val player3 = server.joinLobby(lobbyId)
+    assertEquals(InLobby, player3.state)
+    server.startGame(host)
+    assertInGameWithOneCard(player = host)
+    assertInGameWithOneCard(player = player2)
+    assertInGameWithOneCard(player = player3)
+    val playersSortedByCard = listOf(host, player2, player3).sortedBy { (it.state as InGame).cards[0].value }
+    server.playCard(playersSortedByCard[0])
+    assertInGameWithNoCards(player = playersSortedByCard[0])
+    assertInGameWithOneCard(player = playersSortedByCard[1])
+    assertInGameWithOneCard(player = playersSortedByCard[2])
+    server.playCard(playersSortedByCard[1])
+    assertInGameWithNoCards(player = playersSortedByCard[0])
+    assertInGameWithNoCards(player = playersSortedByCard[1])
+    assertInGameWithOneCard(player = playersSortedByCard[2])
+    server.playCard(playersSortedByCard[2])
+    assertEquals(GameWon, host.state)
+    assertEquals(GameWon, player2.state)
+    assertEquals(GameWon, player3.state)
+  }
+
+  @Test
+  fun `card played in wrong order`() {
+    val server = SimpleServer()
+    val (host, lobbyId) = server.createLobby()
+    assertEquals(InLobby, host.state)
+    val player2 = server.joinLobby(lobbyId)
+    assertEquals(InLobby, player2.state)
+    val player3 = server.joinLobby(lobbyId)
+    assertEquals(InLobby, player3.state)
+    server.startGame(host)
+    assertInGameWithOneCard(player = host)
+    assertInGameWithOneCard(player = player2)
+    assertInGameWithOneCard(player = player3)
+    val playersSortedByCard = listOf(host, player2, player3).sortedBy { (it.state as InGame).cards[0].value }
+    server.playCard(playersSortedByCard[0])
+    assertInGameWithNoCards(player = playersSortedByCard[0])
+    assertInGameWithOneCard(player = playersSortedByCard[1])
+    assertInGameWithOneCard(player = playersSortedByCard[2])
+    server.playCard(playersSortedByCard[2])
+    assertEquals(GameLost, host.state)
+    assertEquals(GameLost, player2.state)
+    assertEquals(GameLost, player3.state)
+  }
+
+  private fun assertInGameWithOneCard(player: Player) {
+    assertIs<InGame>(player.state)
+    val cards = (player.state as InGame).cards
+    assertEquals(1, cards.size)
+    val card = cards[0]
+    assertTrue(card.value >= 1)
+    assertTrue(card.value <= 100)
+  }
+
+  private fun assertInGameWithNoCards(player: Player) {
+    assertIs<InGame>(player.state)
+    val cards = (player.state as InGame).cards
+    assertEquals(0, cards.size)
+  }
+}
