7bc7a407791e4a8c1659479cf03be1fcbd89cfe7
rafearnold <rafe.arnold@gmail.com>
2024-03-09 18:36:00 +0000

    remove duplicate assertions.

--- src/test/kotlin/uk/co/rafearnold/mind/Tests.kt
+++ src/test/kotlin/uk/co/rafearnold/mind/Tests.kt
@@ -40,19 +40,12 @@ class Tests {
   fun `card played in wrong order`() {
     val server = SimpleServer()
     val (host, lobbyId) = server.createLobby()
-    assertEquals(InLobby, host.state)
     val player2 = server.joinLobby(lobbyId)
-    assertEquals(InLobby, player2.state)
     val player3 = server.joinLobby(lobbyId)
-    assertEquals(InLobby, player3.state)
     server.startGame(host)
-    assertInGameWithOneCard(player = host)
-    assertInGameWithOneCard(player = player2)
-    assertInGameWithOneCard(player = player3)
     val players = mutableListOf(host, player2, player3)
     val nextPlayer = players.takeNextPlayer()!!
     server.playCard(nextPlayer)
-    assertInGameWithNoCards(player = nextPlayer)
     players.forEach { assertInGameWithOneCard(player = it) }
     players.takeNextPlayer()!!
     server.playCard(players[0])
